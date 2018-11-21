a0e2cb0ecc1b3a6854ebd1bc6c32d4464060c3cc
Jeff Squyres <jeff@squyres.com>
2018-11-21 15:34:56 -0500

    now shows total clients
    Signed-off-by: Jeff Squyres <jeff@squyres.com>

--- cisco-controller/graph-sqlite3.py
+++ cisco-controller/graph-sqlite3.py
@@ -12,6 +12,7 @@ import re
 
 from matplotlib.ticker import NullFormatter
 from datetime import datetime
+from datetime import date
 from pprint import pformat
 
 #####################################################################
@@ -102,7 +103,7 @@ CREATE TABLE client_sightings (
 
 def graph_num_clients(databases, log):
     # Find the first date in the databases
-    first = None
+    first          = None
     for year, year_db in databases.items():
         for month, month_db in year_db.items():
             for day, day_db in month_db.items():
@@ -115,125 +116,219 @@ def graph_num_clients(databases, log):
 
     log.debug("Found first: {dt}".format(dt=first))
 
+
+    # foo = {
+    #     'minute' : {
+    #         'raw' : {
+    #             minute_timestamp1 : count1,
+    #             minute_timestamp2 : count2,
+    #             minute_timestamp3 : count3,
+    #             ...,
+    #         },
+    #         'x' : list(...),
+    #         'y' : list(...),
+    #     },
+    #     'hour' : {
+    #         'raw' : {
+    #             hour_timestamp1 : { 'total' : total, 'count' : count, 'average' : average },
+    #             hour_timestamp2 : { 'total' : total, 'count' : count, 'average' : average },
+    #             hour_timestamp3 : { 'total' : total, 'count' : count, 'average' : average },
+    #             ...,
+    #         },
+    #         'x' : list(...),
+    #         'y' : list(...),
+    #     },
+    # }
+
+    def _get_empty():
+        return {
+            'minute' : {
+                'raw' : dict(),
+            },
+            'hour' : {
+                'raw' : dict(),
+            },
+        }
+
+    def _normalize_minute(minute):
+        # Normalize to 15-minute increments because
+        # sometimes the controller gets busy and takes 1-3
+        # minutes to report all the stats.
+        if minute < 15:
+            minute = 0
+        elif minute < 30:
+            minute = 15
+        elif minute < 45:
+            minute = 30
+        else:
+            minute = 45
+
+        return minute
+
+    #-----------------------------
     # Now plot them all relative to the first date
-    xaxis       = dict()
-    total       = dict()
-    controllers = dict()
-    aps         = dict()
-    exp         = re.compile("(\d\d\d\d)-(\d\d)-(\d\d) (\d\d):(\d\d):")
+    total          = _get_empty()
+    per_controller = dict()
+    per_ap         = dict()
+    exp            = re.compile("(\d\d\d\d)-(\d\d)-(\d\d) (\d\d):(\d\d):")
     for year, year_db in databases.items():
         for month, month_db in year_db.items():
             for day, day_db in month_db.items():
+
+                # Look at each client sighting at this timestamp.
+                #
+                # Save data both on a per-minute basis, per-hour
+                # basis, and a per-day basis.
                 for _, client in day_db['client_sightings']['rows'].items():
-                    # We take samples at 15 minute intervals, but it
-                    # takes a few seconds to insert all the data into
-                    # the database.  So all the clients seen at a
-                    # given time may not have exactly the same
-                    # timestamp.  For simplicity, just drop the
-                    # seconds off the sqlite3 timestamp -- that's
-                    # "good enough".
-                    ts = client['timestamp']
-                    match = exp.match(ts)
+                    ts_str = client['timestamp']
+                    match  = exp.match(ts_str)
                     if not match:
                         log.error("Malformed timestamp ('{ts}') -- skipped"
-                                  .format(ts=ts))
+                                  .format(ts=ts_str))
                         continue
 
                     year   = int(match.group(1))
                     month  = int(match.group(2))
                     day    = int(match.group(3))
                     hour   = int(match.group(4))
-                    minute = int(match.group(5))
+                    minute = _normalize_minute(int(match.group(5)))
 
-                    ap_id         = client['ap_index']
-                    controller_id = day_db['aps']['rows'][ap_id]['controller_id']
+                    ap_id  = client['ap_index']
+                    c_id   = day_db['aps']['rows'][ap_id]['controller_id']
 
                     dt = datetime(year, month, day, hour, minute)
-                    ts = dt.timestamp()
 
-                    xaxis[ts] = dt
+                    def _minute_increment(data, timestamp):
+                        data_minute = data['minute']['raw']
+                        if not timestamp in data_minute:
+                            data_minute[timestamp] = 0
+                        data_minute[timestamp] += 1
 
-                    if not ts in total:
-                        total[ts] = 0
-                        
-                    if not ap_id in aps:
-                        aps[ap_id] = dict()
-                    if not ts in aps[ap_id]:
-                        aps[ap_id][ts] = 0
-                        
-                    if not controller_id in controllers:
-                        controllers[controller_id] = dict()
-                    if not ts in controllers[controller_id]:
-                        controllers[controller_id][ts] = 0
+                    # Increment the "total number of clients" count at
+                    # this timestamp.
+                    _minute_increment(total, dt)
 
-                    # Add one to the counts
-                    total      [ts]                += 1
-                    aps        [ap_id][ts]         += 1
-                    controllers[controller_id][ts] += 1
+                    # Increment the "number of clients on this AP"
+                    # count at this timestamp.
+                    if not ap_id in per_ap:
+                        per_ap[ap_id] = _get_empty()
+                    _minute_increment(per_ap[ap_id], dt)
+
+                    # Increment the "number of clients on this
+                    # controller" count at this timestamp.
+                    if not c_id in per_controller:
+                        per_controller[c_id] = _get_empty()
+                    _minute_increment(per_controller[c_id], dt)
+
+    #-----------------------------
+    # Iterate over the collected data and compute per-hour averages.
+    def _average_per_hour(data):
+        data_min = data['minute']['raw']
+        for dt_min in data_min:
+            dt_hour = datetime(year=dt_min.year,
+                               month=dt_min.month,
+                               day=dt_min.day,
+                               hour=dt_min.hour)
+
+            data_hour = data['hour']['raw']
+            if dt_hour not in data_hour:
+                data_hour[dt_hour] = {
+                    'total'   : 0,
+                    'count'   : 0,
+                }
+
+            log.debug("Found: min {dtm} --> hour {dth}"
+                      .format(dtm=dt_min, dth=dt_hour))
+            log.debug(pformat(data_min[dt_min]))
+
+            data_hour[dt_hour]['total'] += data_min[dt_min]
+            data_hour[dt_hour]['count'] += 1
+
+        for dt_hour, raw_hour in data_hour.items():
+            average = raw_hour['total'] / raw_hour['count']
+            raw_hour['average'] = average
+
+    _average_per_hour(total)
+    for ap_id in per_ap:
+        _average_per_hour(per_ap[ap_id])
+    for c_id in per_controller:
+        _average_per_hour(per_controller[c_id])
+
+    #-----------------------------
+    def _listize(data, sub_name=None):
+        x = list()
+        y = list()
+
+        sorted_x = sorted(data['raw'])
+        for x_value in sorted_x:
+            x.append(x_value)
+            if sub_name:
+                y.append(data['raw'][x_value][sub_name])
+            else:
+                y.append(data['raw'][x_value])
+
+        data['x'] = x
+        data['y'] = y
 
     # Convert the data into ordered lists
-    x             = list()
-    y_total       = list()
-    sorted_xaxis  = sorted(xaxis)
+    _listize(total['minute'])
+    _listize(total['hour'], 'average')
+    for ap_id in per_ap:
+        _listize(per_ap[ap_id]['minute'])
+        _listize(per_ap[ap_id]['hour'], 'average')
+    for c_id in per_controller:
+        _listize(per_controller[c_id]['minute'])
+        _listize(per_controller[c_id]['hour'], 'average')
 
-    y_controllers = dict()
-    for c_id in controllers:
-        y_controllers[c_id] = list()
-
-    y_aps         = dict()
-    for ap_id in aps:
-        y_aps[ap_id] = list()
-
-    for ts in sorted_xaxis:
-        x.append(xaxis[ts])
-        y_total.append(total[ts])
-
-        for c_id in controllers:
-            if ts in controllers[c_id]:
-                y_controllers[c_id].append(controllers[c_id][ts])
-
-        for ap_id in aps:
-            if ts in aps[ap_id]:
-                y_aps[ap_id].append(aps[ap_id][ts])
-
-    # Plot totals
+    #-----------------------------
+    # Plot total clients
     fig, ax = plt.subplots()
     ax.set(xlabel='Days', ylabel='Number of clients',
            title='Total number of wifi clients')
 
     log.info("Plotting total number of clients on all controllers")
-    ax.plot(x, y_total, label='Total clients')
+    # JMS This makes a very busy plot
+    #ax.plot(total['minute']['x'], total['minute']['y'],
+    #        label='Total clients')
+    ax.step(total['hour']['x'], total['hour']['y'],
+            label='Averge clients per hour')
 
+    plt.legend()
     ax.get_xaxis().set_major_locator(mdates.DayLocator(interval=1))
     ax.get_xaxis().set_major_formatter(mdates.DateFormatter("%a %b %d"))
     ax.grid()
-    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+    plt.setp(ax.get_xticklabels(), rotation=20, ha="right",
          rotation_mode="anchor")
 
     fig.savefig("total-clients-on-all-controllers.pdf")
     #plt.show()
 
+    #-----------------------------
     # Plot clients on each controller
     fig, ax = plt.subplots()
     ax.set(xlabel='Days', ylabel='Number of clients',
-           title='Number of wifi clients on each controller')
+           title='Average number of wifi clients per hour on each controller')
 
-    for c_id in controllers:
-        log.info("Ploting number of clients on controller {id}".format(id=c_id))
-        log.info("Len of X: {lx}, Len of Y: {ly}"
-                 .format(lx=len(x), ly=len(y_controllers[c_id])))
-        continue
-        ax.plot(x, y_controllers[c_id], label='Clients on controller {id}'.format(id=c_id))
+    colors = ['r', 'b']
+    i = 0
+    for c_id, c in per_controller.items():
+        log.info("Ploting average number of clients per hour on controller {id}"
+                 .format(id=c_id))
+        ax.plot(c['hour']['x'], c['hour']['y'], colors[i],
+                label='Average clients per hour on controller {id}'.format(id=c_id))
+        i += 1
 
+    plt.legend()
     ax.get_xaxis().set_major_locator(mdates.DayLocator(interval=1))
     ax.get_xaxis().set_major_formatter(mdates.DateFormatter("%a %b %d"))
     ax.grid()
-    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+    plt.setp(ax.get_xticklabels(), rotation=20, ha="right",
          rotation_mode="anchor")
 
     fig.savefig("total-clients-on-each-controller.pdf")
     plt.show()
-    
+
+    #-----------------------------
 
 #####################################################################
 
@@ -286,7 +381,7 @@ def read_database(filename, log):
     db['clients']          = read_table(cur, 'clients', log)
     db['client_sightings'] = read_table(cur, 'client_sightings', log)
     db['ap_sightings']     = read_table(cur, 'ap_sightings', log)
-    
+
     conn.close()
 
     return db
@@ -366,11 +461,11 @@ def setup_cli():
         print("Error: directory '{dir}' does not exist"
               .format(dir=args.dir))
         exit(1)
-        
+
     return args
 
 #####################################################################
-    
+
 def main():
     args      = setup_cli()
     log       = setup_logging(args)
